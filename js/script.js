a91ee9ff115698d1705778442cf851e766a1284c
volstep <s.volstep@gmail.com>
2020-08-16 16:19:17 +0300

    012

--- js/script.js
+++ js/script.js
@@ -1,3 +1,19 @@
 "use strict";
-let n=5;
-alert(`n=${n}`);
+let numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?", "");
+
+let personMovieDB = {
+    count: +numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+let lastFilm = prompt("Один из последних фильмов?", "");
+let rating = prompt("На сколько оцените его?", "");
+let lastFilm1 = prompt("Один из последних фильмов?", "");
+let rating1 = prompt("На сколько оцените его?", "");
+
+personMovieDB.movies[lastFilm] = rating;
+personMovieDB.movies[lastFilm1] = rating1;
+console.log(personMovieDB);
